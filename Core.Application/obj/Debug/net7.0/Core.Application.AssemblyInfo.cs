1e3f91ec13b8fd358b03fe0d5e631c1aca0af763
AbdulhakimKaya <abdulhakimkaya2199@gmail.com>
2024-02-21 08:41:21 +0300

    Global Hata Yönetimi

--- Core.Application/obj/Debug/net7.0/Core.Application.AssemblyInfo.cs
+++ Core.Application/obj/Debug/net7.0/Core.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3defe1263f05711f01002b72a1e45104ad2d72ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c93b2d1dd7191fa19f02e6ce22b5f27cd06c332b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
