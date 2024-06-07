9d2aea5c3f823d537f64a84620ed97e180e546fc
AbdulhakimKaya <abdulhakimkaya2199@gmail.com>
2024-06-07 16:47:41 +0300

    Security Helpers and Extensions

--- Core.Persistence/obj/Debug/net7.0/Core.Persistence.AssemblyInfo.cs
+++ Core.Persistence/obj/Debug/net7.0/Core.Persistence.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Core.Persistence")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fd37d1462d6f45fba0c6fb64c70cf05f6d6ee3c1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc1847089d66f0396b2a197e057671f0a742b822")]
 [assembly: System.Reflection.AssemblyProductAttribute("Core.Persistence")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Core.Persistence")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
